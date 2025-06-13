16fae6e7682809a25d85c172413564d2af165f15
nooya1215 <nooya1215@gmail.com>
2025-06-13 10:01:18 +0900

    페이지 이동시 슬릭 작동

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import title1 from "../assets/img/svg/rectangle.svg";
 import "../assets/css/title.css";
-import "../assets/js/title.js";
-import title1 from "../assets/img/svg/rectangle.svg"
+
 
 export default function Title() {
+  useEffect(() => {
+    const $slider = window.$('.title-slide');
+
+    if ($slider.hasClass('slick-initialized')) {
+      $slider.slick('unslick');
+    }
+
+    $slider.slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      fade: true,
+      cssEase: 'linear',
+      customPaging: function (slider, i) {
+        return (
+          '<span class="custom-dot">' + (i + 1) + ' / ' + slider.slideCount + '</span>'
+        );
+      },
+    });
+  }, []); // 마운트 시 한 번만 실행
+
   return (
     <div id="title">
       <div className="wrap">
-        <ul className='title-slide'>
+        <ul className="title-slide">
           <li>
             <img src={title1} alt="타이틀 이미지" />
             <h2 className="h2">1:D CLASS는<br />ONE DAY CLASS입니다</h2>
