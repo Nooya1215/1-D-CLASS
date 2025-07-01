d0aa382806b32d9fe87bb3d2b8bafd734af1d158
nooya1215 <nooya1215@gmail.com>
2025-07-01 14:44:24 +0900

    디테일 및 번역안된부분 번역

--- frontend/src/pages/WishListPage.jsx
+++ frontend/src/pages/WishListPage.jsx
@@ -5,26 +5,30 @@ import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 import '../assets/css/card-basic.css';
 import '../assets/css/WishListPage.css';
+import { useTranslation } from 'react-i18next';
 
 const WishList = () => {
+  const { t } = useTranslation();
   const { wishList } = useWish();
 
   return (
-    <div className='wrap'>
-      <h2>관심 있는 클래스</h2>
-      <p>언젠가 해보고 싶은 클래스를 모아봤어요.</p>
-      <ul className="product-list-container">
-        {wishList.length === 0 ? (
-          <li className="empty-list">
-            <p>아직 찜한 클래스가 없어요!</p>
-            <p>마음에 드는 클래스를<span>눌러 담아보세요.</span></p>
-            <Link to="/products/all" className='btn'>클래스 둘러보기</Link>
-          </li>
-        ) : (
-          wishList.map((product) => <Card key={product.id} product={product} />)
-        )}
-      </ul>
-    </div>
+    <section id='Wishlist'>
+      <div className='wrap'>
+        <h2>{t('wishList.title')}</h2>
+        <p>{t('wishList.description')}</p>
+        <ul className="product-list-container">
+          {wishList.length === 0 ? (
+            <li className="empty-list">
+              <p>{t('wishList.emptyMessage1')}</p>
+              <p>{t('wishList.emptyMessage2')}<span>{t('wishList.emptyMessage2Span')}</span></p>
+              <Link to="/products/all" className='btn'>{t('wishList.browseBtn')}</Link>
+            </li>
+          ) : (
+            wishList.map((product) => <Card key={product.id} product={product} />)
+          )}
+        </ul>
+      </div>
+    </section>
   );
 };
 
